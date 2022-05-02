6796c1162036da83b81c088113e24da5fa132999
rpancham <rpancham@fedora.com>
2022-05-02 15:52:46 +0530

    added correct copy of fish service

--- src/main/java/io/opentelemetry/examples/fish/FishApplication.java
+++ src/main/java/io/opentelemetry/examples/fish/FishApplication.java
@@ -1,15 +1,15 @@
-//package io.opentelemetry.examples.fish.animals;
-//
-//import io.opentelemetry.examples.utils.OpenTelemetryConfig;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class FishApplication {
-//
-//  public static void main(String[] args) {
-//    // Configure OpenTelemetry as early as possible
-//    OpenTelemetryConfig.configureGlobal("disney-app");
-//    SpringApplication.run(FishApplication.class, args);
-//  }
-//}
+package io.opentelemetry.examples.fish.animals;
+
+import io.opentelemetry.examples.utils.OpenTelemetryConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishApplication {
+
+  public static void main(String[] args) {
+    // Configure OpenTelemetry as early as possible
+    OpenTelemetryConfig.configureGlobal("disney-app");
+    SpringApplication.run(FishApplication.class, args);
+  }
+}
