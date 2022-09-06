8d9080a18ae64871cc586e2d5fab8205cdfa8eb5
Ben Evans <benjamin.john.evans@gmail.com>
2022-09-06 17:55:17 +0200

    Surely not needing the OTel bean?

--- src/main/java/io/opentelemetry/examples/mammal/MammalApplication.java
+++ src/main/java/io/opentelemetry/examples/mammal/MammalApplication.java
@@ -1,12 +1,28 @@
 package io.opentelemetry.examples.mammal;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 
-    @SpringBootApplication
+import java.util.Optional;
+
+@SpringBootApplication
     public class MammalApplication {
 
-        public static void main(String[] args) {
+    @Bean
+    @ConditionalOnClass(name = "io.opentelemetry.javaagent.OpenTelemetryAgent")
+    public MeterRegistry otelRegistry() {
+        Optional<MeterRegistry> otelRegistry = Metrics.globalRegistry.getRegistries().stream()
+                .filter(r -> r.getClass().getName().contains("OpenTelemetryMeterRegistry"))
+                .findAny();
+        otelRegistry.ifPresent(Metrics.globalRegistry::remove);
+        return otelRegistry.orElse(null);
+    }
+
+    public static void main(String[] args) {
             SpringApplication.run(MammalApplication.class, args);
         }
     }
